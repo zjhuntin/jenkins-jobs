a1243291ae01b8750b3d78f47f16aed7dcb7c1f8
Evgeni Golov <evgeni@golov.de>
2021-04-12 12:03:37 +0200

    add focal to debian pipelines

--- theforeman.org/pipelines/vars/foreman/nightly.groovy
+++ theforeman.org/pipelines/vars/foreman/nightly.groovy
@@ -11,12 +11,13 @@ def foreman_el_releases = [
     'el7',
     'el8'
 ]
-def foreman_debian_releases = ['buster', 'bionic']
+def foreman_debian_releases = ['buster', 'bionic', 'focal']
 
 def pipelines_deb = [
     'install': [
         'debian10',
-        'ubuntu1804'
+        'ubuntu1804',
+        'ubuntu2020'
     ],
     'upgrade': [
         'debian10',
