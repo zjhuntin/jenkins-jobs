1d50d051367a6c73a6a2430c1235bf22f59ad7aa
Evgeni Golov <evgeni@golov.de>
2021-04-12 12:07:53 +0200

    push nightly debs based on foreman_debian_releases
    not a hardcoded list

--- theforeman.org/pipelines/release/pipelines/foreman-deb.groovy
+++ theforeman.org/pipelines/release/pipelines/foreman-deb.groovy
@@ -22,10 +22,18 @@ pipeline {
             agent { label 'debian' }
 
             steps {
-                parallel(
-                    "buster": { push_debs_direct('buster', 'nightly') },
-                    "bionic": { push_debs_direct('bionic', 'nightly') }
-                )
+                script {
+                    def pushDistros = [:]
+                    foreman_debian_releases.each { distro ->
+                        pushDistros["push-${foreman_version}-${distro}"] = {
+                            script {
+                                push_debs_direct(distro, foreman_version)
+                            }
+                        }
+                    }
+
+                    parallel pushDistros
+                }
             }
         }
     }
