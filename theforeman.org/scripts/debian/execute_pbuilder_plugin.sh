c17bf1b678b295c446f21e2261b25cac7a0a4c0d
Evgeni Golov <evgeni@golov.de>
2021-04-23 07:38:22 +0200

    support building non-gem plugins

--- theforeman.org/scripts/debian/execute_pbuilder_plugin.sh
+++ theforeman.org/scripts/debian/execute_pbuilder_plugin.sh
@@ -8,9 +8,29 @@ echo "--Setting Up Sources"
 cd plugins
 mkdir build-${project}
 cd build-${project}
-cp -r ../${project} ./
-cd ${project}
-../../download_gems
+
+BUILD_TYPE=gem
+# Import variables from the project, allowing it to override behaviour
+if [ -e ../${project}/build_vars.sh ]; then
+  . ../${project}/build_vars.sh
+fi
+
+VERSION=$(head -n1 ../${project}/debian/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-|cut -d: -f2)
+if [[ $BUILD_TYPE == "gem" ]]; then
+  cp -r ../${project} ./
+  cd ${project}
+  ../../download_gems
+elif [[ $BUILD_TYPE == "ansible-collection" ]];
+  COLLECTION=${project#ansible-collection-}
+  wget "https://galaxy.ansible.com/download/${COLLECTION}-${VERSION}.tar.gz"
+  mv "${COLLECTION}-${VERSION}.tar.gz" "${project}_${VERSION}.orig.tar.gz"
+  cp -r ../${project} ./
+  tar -x -C "${project}" --strip-components=1 -f "${project}_${VERSION}.orig.tar.gz"
+  cd ${project}
+else
+  echo "Unsupported build type: ${BUILD_TYPE}"
+  exit 1
+fi
 
 # Add changelog entry if this is a git/nightly build
 if [ x$gitrelease = xtrue ] || [ x$pr_number != x ]; then
