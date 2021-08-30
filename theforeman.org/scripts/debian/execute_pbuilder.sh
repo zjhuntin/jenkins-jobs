fb7ef1c12f586bdc859d55708bcd9098f8a465c6
Evgeni Golov <evgeni@golov.de>
2021-08-30 15:29:28 +0200

    hooks cleanup (#98)
    * drop static hooks
    
    these are now deployed by puppet
    
    * use a local hookdir for setting up the foreman repo
    
    * add stagingdeb for core, to pull in new dependencies
    
    * drop configure_pbuilder

--- theforeman.org/scripts/debian/execute_pbuilder.sh
+++ theforeman.org/scripts/debian/execute_pbuilder.sh
@@ -6,7 +6,7 @@ echo "--Execute pdebuild"
 
 # Build the package for the OS using pbuilder
 # needs sudo as pedebuild uses loop and bind mounts
-sudo pdebuild-${os}64
+sudo FOREMAN_VERSION=${version} pdebuild-${os}64
 
 # Cleanup, pdebuild uses root
 sudo chown -R jenkins:jenkins $WORKSPACE
