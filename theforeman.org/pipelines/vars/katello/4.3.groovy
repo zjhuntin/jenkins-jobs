422e7f6973ce9b9e6a3bea31ce51916f94dd3ce9
Odilon Sousa <osousa@redhat.com>
2021-11-16 15:07:59 -0300

    Preparing for Katello 4.3

---
+++ theforeman.org/pipelines/vars/katello/4.3.groovy
@@ -0,0 +1,17 @@
+def foreman_version = '3.1'
+def katello_version = '4.3'
+def foreman_el_releases = [
+    'el7',
+    'el8'
+]
+def pipelines = [
+    'install': [
+        'centos7',
+        'centos8',
+        'centos8-stream',
+    ],
+    'upgrade': [
+        'centos7',
+        'centos8',
+    ]
+]
