7c6b2b73443f7f984e354643f934e9f49a171849
Odilon Sousa <osousa@redhat.com>
2022-05-09 09:53:29 -0300

    Adding configs for Foreman 3.3

---
+++ theforeman.org/pipelines/vars/foreman/3.3.groovy
@@ -0,0 +1,42 @@
+def foreman_version = '3.3'
+def foreman_client_distros = [
+    'el8',
+    'el7',
+    'sles12',
+    'sles11'
+]
+def foreman_el_releases = [
+    'el7',
+    'el8'
+]
+def foreman_debian_releases = ['buster', 'bullseye', 'focal']
+
+def pipelines_deb = [
+    'install': [
+        'debian10',
+        'debian11',
+        'ubuntu2004'
+    ],
+    'upgrade': [
+        'debian10',
+        'ubuntu2004'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos7',
+        'centos8-stream',
+        'almalinux8',
+    ],
+    'upgrade': [
+        'centos7',
+        'centos8-stream',
+        'almalinux8',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]
