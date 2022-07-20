f8df06667f0248c72cc92bdfbc487024a1dd8685
Eric Helms <ericdhelms@gmail.com>
2022-07-20 13:54:39 -0400

    Katello branch should be a string (#245)

--- theforeman.org/pipelines/test/foreman/vars.groovy
+++ theforeman.org/pipelines/test/foreman/vars.groovy
@@ -1,19 +1,19 @@
 def foreman_integration_versions = [
     'develop': [
         'ruby': ['2.7'],
-        'katello': ['master']
+        'katello': 'master'
     ],
     '3.3-stable': [
         'ruby': ['2.7'],
-        'katello': ['KATELLO-4.5']
+        'katello': 'KATELLO-4.5'
     ],
     '3.2-stable': [
         'ruby': ['2.7'],
-        'katello': ['KATELLO-4.4']
+        'katello': 'KATELLO-4.4'
     ],
     '3.1-stable': [
         'ruby': ['2.7'],
-        'katello': ['KATELLO-4.3']
+        'katello': 'KATELLO-4.3'
     ]
 ]
 
