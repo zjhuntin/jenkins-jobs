zjhuntin/jenkins-jobs

commit d3307cea3fffbb1da32d8b8b9e5b2413201f3913

Install cicoclient using pip
In the most recent cico-workspace container image the Ansible version
was updated to ansible-core. This is using Python 3.8 while the
python-cicoclient package is installed using Python 3.6.

The intended replacement is duffy, but for now this allows us to
provision.

This also changes the pip install command to use pip3.8.

centos.org/pipelines/lib/foremanCentosJob.groovy
@@ -19,9 +19,7 @@ pipeline {
                 deleteDir()
                 git url: 'https://github.com/theforeman/forklift.git'
 
-                // old pip doesn't use binary wheels, and we really do not want to compile the otel sdk
-                sh(label: 'update pip', script: 'pip3 install -U pip --user')
-                sh(label: 'pip install', script: '~/.local/bin/pip install opentelemetry-api opentelemetry-sdk opentelemetry-exporter-otlp')
+                sh(label: 'pip install', script: 'pip3.8 install opentelemetry-api opentelemetry-sdk opentelemetry-exporter-otlp cicoclient')
             }
         }
         stage('Provision Node') {

centos.org/ansible/ansible.cfg
@@ -1,5 +1,5 @@
 [defaults]
 # Attempt to load cico Ansible module whether it's installed system-wide or from a virtual environment
-library = /usr/lib/python2.7/site-packages/cicoclient/ansible:$VIRTUAL_ENV/lib/python2.7/site-packages/cicoclient/ansible:/usr/lib/python3.6/site-packages/cicoclient/ansible:$VIRTUAL_ENV/lib/python3.6/site-packages/cicoclient/ansible
+library = $VIRTUAL_ENV/lib/python3.8/site-packages/cicoclient/ansible:/usr/lib/python3.6/site-packages/cicoclient/ansible
 host_key_checking = False
 retry_files_enabled = False

commit 3e3e1aea8fa3fa5f4c970d44188852500e5e6627

Correct cicoclient package name
It's https://pypi.org/project/python-cicoclient not cicoclient.

Fixes: d3307cea3fffbb1da32d8b8b9e5b2413201f3913

centos.org/pipelines/lib/foremanCentosJob.groovy
@@ -19,7 +19,7 @@ pipeline {
                 deleteDir()
                 git url: 'https://github.com/theforeman/forklift.git'
 
-                sh(label: 'pip install', script: 'pip3.8 install opentelemetry-api opentelemetry-sdk opentelemetry-exporter-otlp cicoclient')
+                sh(label: 'pip install', script: 'pip3.8 install opentelemetry-api opentelemetry-sdk opentelemetry-exporter-otlp python-cicoclient')
             }
         }
         stage('Provision Node') {