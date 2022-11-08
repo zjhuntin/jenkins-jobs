zjhuntin/jenkins-jobs

commit 743e37d88303858ff2acf830d9708ee587c2057c

Add Foreman 3.5 jobs/configs

theforeman.org/pipelines/vars/foreman/3.5.groovy
@@ -1,0 +1,39 @@
+def foreman_version = 'nightly'
+def foreman_client_distros = [
+    'el9',
+    'el8',
+    'el7',
+    'sles12',
+    'sles11'
+]
+def foreman_el_releases = [
+    'el8'
+]
+def foreman_debian_releases = ['bullseye', 'focal']
+
+def pipelines_deb = [
+    'install': [
+        'debian11',
+        'ubuntu2004'
+    ],
+    'upgrade': [
+        'debian11',
+        'ubuntu2004'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos8-stream',
+        'almalinux8',
+    ],
+    'upgrade': [
+        'centos8-stream',
+        'almalinux8',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]

centos.org/jobs/foreman-pipelines.yml
@@ -10,6 +10,7 @@
     version:
       - '3.3'
       - '3.4'
+      - '3.5'
       - nightly
     type:
       - foreman

theforeman.org/yaml/includes/foreman_versions.yaml.inc
@@ -1,2 +1,3 @@
 - '3.3'
 - '3.4'
+- '3.5'

theforeman.org/yaml/jobs/test_3_5_stable.yaml
@@ -1,0 +1,38 @@
+- job:
+    name: test_3_5_stable
+    project-type: matrix
+    quiet-period: 2700
+    properties:
+      - github_foreman
+    scm:
+      - foreman:
+          branch: '3.5-stable'
+    triggers:
+      - scm_fifteen_minutes
+      - github
+    axes:
+      - axis:
+          type: user-defined
+          name: ruby
+          values:
+            - 2.7
+      - axis:
+          type: user-defined
+          name: database
+          values:
+            - postgresql
+      - axis:
+          type: label-expression
+          name: slave
+          values:
+            - fast
+    builders:
+      - test_develop
+    publishers:
+      - gemset_cleanup
+      - ircbot_freenode
+      - archive:
+          artifacts: 'pkg/*,Gemfile.lock,package-lock.json'
+          only-if-success: false
+      - junit:
+          results: 'jenkins/reports/unit/*.xml'

theforeman.org/yaml/jobs/test_proxy_3_5_stable.yaml
@@ -1,0 +1,28 @@
+- job:
+    name: test_proxy_3_5_stable
+    project-type: matrix
+    properties:
+      - github:
+          url: https://github.com/theforeman/smart-proxy
+    scm:
+      - foreman-proxy:
+          branch: '3.5-stable'
+    triggers:
+      - scm_fifteen_minutes
+      - github
+    axes:
+      - axis:
+          type: user-defined
+          name: ruby
+          values:
+          - 2.7
+    builders:
+      - foreman-proxy
+    publishers:
+      - gemset_cleanup
+      - ircbot_freenode
+      - junit:
+          results: 'jenkins/reports/unit/*.xml'
+      - archive:
+          artifacts: 'pkg/*'
+          only-if-success: true

theforeman.org/yaml/views/release.yml
@@ -13,6 +13,7 @@
     minor:
         - '3'
         - '4'
+        - '5'
 
 - view:
     name: 'Foreman Nightly'

commit 7cda9bf07f2547298bd6c3883d013b3e12acab17

Fix Foreman version in 3.5 vars
Fixes: 743e37d88303858ff2acf830d9708ee587c2057c

theforeman.org/pipelines/vars/foreman/3.5.groovy
@@ -1,4 +1,4 @@
-def foreman_version = 'nightly'
+def foreman_version = '3.5'
 def foreman_client_distros = [
     'el9',
     'el8',