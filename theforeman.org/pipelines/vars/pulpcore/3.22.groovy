d723cfd01f7bd84ef16ffbf427afbc30c587f213
Odilon Sousa <osousa@redhat.com>
2023-02-01 12:12:12 -0300

    Add Config for Pulpcore 3.22

---
+++ theforeman.org/pipelines/vars/pulpcore/3.22.groovy
@@ -0,0 +1,9 @@
+def pulpcore_version = '3.22'
+def pulpcore_distros = ['el8', 'el9']
+def packaging_branch = 'rpm/3.22'
+def pipelines = [
+    'pulpcore': [
+        'centos8-stream',
+        'centos9-stream'
+    ]
+]
