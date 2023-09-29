c0fdf9013b6d6a57deea54ca4719d5c485f280e5
Eric D. Helms <ericdhelms@gmail.com>
2023-09-29 08:26:39 -0400

    Add Copr jobs for pulpcore-packaging

---
+++ theforeman.org/pipelines/vars/pulpcore/nightly.groovy
@@ -0,0 +1,9 @@
+def pulpcore_version = 'nightly'
+def pulpcore_distros = ['el8', 'el9']
+def packaging_branch = 'rpm/develop'
+def pipelines = [
+    'pulpcore': [
+        'centos8-stream',
+        'centos9-stream'
+    ]
+]
