55c177cca3c740e7dae231b3fbcb7f51025f1e53
Zach Huntington-Meath <zhunting@redhat.com>
2023-11-20 10:47:41 -0500

    Add 3.9 and 4.11 jobs for branching

---
+++ theforeman.org/pipelines/vars/katello/4.11.groovy
@@ -0,0 +1,16 @@
+def foreman_version = '3.9'
+def katello_version = '4.11'
+def stage_source = 'copr'
+def foreman_el_releases = [
+    'el8'
+]
+def pipelines = [
+    'install': [
+        'centos8-stream',
+        'almalinux8',
+    ],
+    'upgrade': [
+        'centos8-stream',
+        'almalinux8',
+    ]
+]
