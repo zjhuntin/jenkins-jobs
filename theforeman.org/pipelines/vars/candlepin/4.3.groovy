5b9fcb8536bb3ea0becf8f6fa077990085fef9af
Eric D. Helms <ericdhelms@gmail.com>
2024-02-12 09:04:47 -0500

    Add EL9 to Candlepin 4.3

--- theforeman.org/pipelines/vars/candlepin/4.3.groovy
+++ theforeman.org/pipelines/vars/candlepin/4.3.groovy
@@ -1,8 +1,9 @@
 def candlepin_version = '4.3'
-def candlepin_distros = ['el8']
+def candlepin_distros = ['el8', 'el9']
 def packaging_branch = 'rpm/4.3'
 def pipelines = [
     'candlepin': [
-        'centos8-stream'
+        'centos8-stream',
+        'centos9-stream'
     ]
 ]
