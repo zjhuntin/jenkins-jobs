866c58ff23761e25a46972d8dca9c97128e2baf2
Zach Huntington-Meath <zhunting@redhat.com>
2024-05-20 13:18:57 -0400

    Foreman 3.11 Jobs/Pipelines

---
+++ theforeman.org/pipelines/vars/foreman/3.11.groovy
@@ -0,0 +1,45 @@
+def foreman_version = '3.11'
+def git_branch = "${foreman_version}-stable"
+
+def foreman_client_distros = [
+    'el9',
+    'el8',
+    'el7'
+]
+def foreman_el_releases = [
+    'el9',
+    'el8'
+]
+def foreman_debian_releases = ['bullseye', 'bookworm', 'focal', 'jammy']
+
+def pipelines_deb = [
+    'install': [
+        'debian11',
+        'ubuntu2004',
+        'ubuntu2204'
+    ],
+    'upgrade': [
+        'debian11',
+        'ubuntu2004'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos8-stream',
+        'centos9-stream',
+        'almalinux8',
+        'almalinux9',
+    ],
+    'upgrade': [
+        'centos8-stream',
+        'centos9-stream',
+        'almalinux8',
+        'almalinux9',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]
