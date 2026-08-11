0695e887453a88f3acf0cdfffc513a8cc81b1ccd
Odilon Sousa <osousa@redhat.com>
2026-08-11 19:11:08 -0300

    Add EL10 to pulpcore 3.105

--- theforeman.org/pipelines/vars/pulpcore/3.105.groovy
+++ theforeman.org/pipelines/vars/pulpcore/3.105.groovy
@@ -1,8 +1,10 @@
 def pulpcore_version = '3.105'
-def pulpcore_distros = ['el9']
+def pulpcore_distros = ['el10', 'el9']
 def packaging_branch = 'rpm/3.105'
 def pipelines = [
     'pulpcore': [
+        'centos10-stream',
+        'almalinux10',
         'centos9-stream',
         'almalinux9',
     ]
